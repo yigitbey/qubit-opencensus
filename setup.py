7811a287262f31f79ae2c2902380121ec8df0caa
Tristan Colgate <tristan@qubit.com>
2018-08-06 11:58:05 +0100

    Update version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ namespaces = ['qubit']
 
 setup(
     name='qubit-opencensus',
-    version='0.1.12',
+    version='0.1.13',
     author='Qubit',
     author_email='tristan@qubit.com',
     classifiers=[
